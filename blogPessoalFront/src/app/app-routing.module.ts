0f134dd6632a54bc969c93808c74751a9f126f6c
RafaelBalbino <rafaelballabi@hotmail.com>
2022-05-30 10:04:07 -0300

    Atualizado em 30.05.2022 | Adc Rotas

--- blogPessoalFront/src/app/app-routing.module.ts
+++ blogPessoalFront/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { /* Rota fixa, redirecionando para 'entrar' e dar um varredura, 
+     * checar se a rota realmente existe. */
+    path: '', redirectTo: 'entrar', pathMatch: 'full'
+  },
+
+  { /* Conecta um componente em uma rota, quando formos para a rota
+     * 'entrar', para poder navegar entre os componentes. */
+    path: 'entrar', component: EntrarComponent
+  },
+
+  { /* Conecta um componente em uma rota, quando formos para a rota
+     * 'cadastrar', para poder navegar entre os componentes. */
+    path: 'cadastrar', component: CadastrarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
